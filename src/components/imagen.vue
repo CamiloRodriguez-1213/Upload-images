9674b21ee1233b96c03e4f98ad0f3183f906d070
Camilo Rodriguez <andrescamilocr482@gmail.com>
2020-10-06 23:56:50 -0500

    Upload Images

---
+++ src/components/imagen.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card">
+          <img class="img-fluid" :src="imagen.url" alt="">
+          <div class="card-body">
+            <h5 class="card-title"><p>{{imagen.created_at}}</p></h5>
+          </div>
+        </div>
+</template>
+<script>
+export default {
+    name: "Imagen",
+    props: ["imagen"]
+}
+</script>
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container-fluid">
+    
+    <div class="row">
+      
+      <div class="col-12">
+        
+        <div class="card tarjeta">
+          <br><br>
+          <h1 class="text-center">Subir archivo</h1>
+          <div class="card-footer">
+            
+              <input  style="display: 150px;" class="carga py-5 pl-5" type="file" @change="onFileSelected($event)">
+              <br><br>
+            
+            <div class="row justify-content-center">
+        <button type="submit" @click="onUpload" style="width:15rem" class="btn btn-primary">Subir imagen</button>
+      
+    </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Home',
+    data(){
+      return{
+        selectedFile: null,
+      }
+    },
+    methods: {
+      onFileSelected(event){
+        this.selectedFile = event.target.files[0]
+      },
+      onUpload(){
+        const fd = new FormData();
+        fd.append('url', this.selectedFile,this.selectedFile.name)
+        axios.post("https://api.jeisontech.dev/api/images/",fd)
+        .then(res => {
+          console.log(res)
+        })
+      }
+}
+};
+
+</script>
+
+<style>
+.tarjeta{
+  width: 50rem;
+  height: 12rem ;
+  border: none;
+}
+.card-footer{
+  text-align: center;
+  background-color: white;
+  border: none;
+  
+}
+.carga{
+  
+  height: 150px;
+  width: 500px;
+  background-color: white;
+  border: 1px solid rgb(0, 0, 0);
+  
+  
+}
+.carga :focus{
+  border: none;
+  
+}
+
+</style>
